ed32067800806bdd8ca03e7bafe44cdafecfd209
Yurii Prodan <yuriidevac@gmail.com>
2021-05-19 11:06:49 +0200

    add random background color to user avatar

--- src/resourses/imageGenerator.js
+++ src/resourses/imageGenerator.js
@@ -1,5 +1,14 @@
 const textToImage = require("text-to-image");
 
+const colors = [
+  "#66ff33",
+  "#00ffcc",
+  "#00ffff",
+  "#ff9966",
+  "#ccccff",
+  "#00ccff",
+  "#ff66cc",
+];
 async function generateImage(username) {
   const dataUri = await textToImage.generate(username[0].toUpperCase(), {
     debug: false,
@@ -8,7 +17,7 @@ async function generateImage(username) {
     fontSize: 30,
     fontFamily: "Verdana",
     margin: 2,
-    bgColor: "blue",
+    bgColor: colors[Math.floor(Math.random() * 7)],
     textColor: "white",
     textAlign: "center",
     verticalAlign: "top",
